c42a9d437fbfab3328f2de2f3262edcc2abbd85d
gorkavillar <gorka@gorkavillar.com>
2023-01-25 12:22:34 +0100

    🎀🎀🎀 Tercer decorador creado

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,43 @@ export const logMethod = (target: any, propertyKey: string, descriptor: Property
 }
 
 // 2 - Medir el tiempo que tarda un método en ejecutarse
+export const measureTime = (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
+    const metodoOriginal = descriptor.value
+
+    descriptor.value = function(...args: any) {
+        const ahora = Date.now()
+        const resultado = metodoOriginal.apply(this, args)
+        const luego = Date.now()
+        console.log(`El método ${propertyKey} ha tardado ${luego - ahora}ms`)
+        return resultado
+    }
+}
 
 // 3 - Almacenar en caché la información de un valor
+type CacheObject = { [key: string]: any }
+
+export const cacheableMethod = (cache: CacheObject) => {
+    return function(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        const metodoOriginal = descriptor.value
+
+        descriptor.value = function(...args: any[]) {
+            const key = JSON.stringify({ target, propertyKey, args })
+            if (cache[key]) {
+                console.log(`Estoy retornando el resultado cacheado para el método ${propertyKey}`)
+                return cache[key]
+            }
+            console.log(`Estoy retornando el resultado nuevo para el método ${propertyKey}`)
+            const resultado = metodoOriginal.apply(this, args)
+
+            cache[key] = resultado
+            console.log("key", key)
+
+            return resultado
+        }
+    }
+}
+
+// const cache: CacheObject = {}
 
 // class Cantante {
 //     nombre: string
@@ -25,13 +60,18 @@ export const logMethod = (target: any, propertyKey: string, descriptor: Property
 //         this.estrofa = e
 //     }
 
-//     @logMethod
+//     @measureTime
+//     @cacheableMethod(cache)
 //     canta() {
 //         console.log(this.estrofa)
+//         for (let i = 0; i < 1000000000; i++) { let a = 0 }
 //         return this.estrofa
 //     }
 // }
 
 // const miCantante = new Cantante("Kurt Cobain", "Load up on guns, bring your friends")
 
+// miCantante.canta()
+// miCantante.canta()
+// miCantante.canta()
 // miCantante.canta()
